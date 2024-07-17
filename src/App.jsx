b09373e8f7433674bf66769f0b75740c8cf8fc77
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-07-17 18:10:51 +0400

    implement query dev tool

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Posts } from "./Posts";
 import "./App.css";
 // we need to import query client and query client provider
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import react query devtools which is only running in development mode
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 // we need to create new query client
 const queryClient = new QueryClient();
@@ -14,6 +16,7 @@ function App() {
         <h1>Blog &apos;em Ipsum</h1>
         <Posts />
       </div>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
