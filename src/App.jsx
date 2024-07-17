1b26cbb220c8de781bc8551c260ca101f84171eb
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-07-17 17:06:56 +0400

    implement query client and query client provider in our application

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { Posts } from "./Posts";
 import "./App.css";
+// we need to import query client and query client provider
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// we need to create new query client
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    // provide React Query client to App
-    <div className="App">
-      <h1>Blog &apos;em Ipsum</h1>
-      <Posts />
-    </div>
+    // we are now providing query client to our full application
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Blog &apos;em Ipsum</h1>
+        <Posts />
+      </div>
+    </QueryClientProvider>
   );
 }
 
