3a35b973a7c9adbb0a3dc0d11dfc15af8340fe0d
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-07-18 12:36:20 +0400

    implement query for post comments

--- src/PostDetail.jsx
+++ src/PostDetail.jsx
@@ -1,9 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
 import { fetchComments } from "./api";
 import "./PostDetail.css";
 
 export function PostDetail({ post }) {
-  // replace with useQuery
-  const data = [];
+  // we are using use query to get data and other functions
+  const { data, isLoading, isError, error } = useQuery({
+    // we have key of the data
+    queryKey: [post.id],
+    // here we need to callBack cuz we need to pass post id
+    queryFn: () => fetchComments(post.id),
+  });
+
+  if (isLoading) return <h1>Loading comments...</h1>;
+
+  if (isError) return <h2>{error.message}</h2>;
 
   return (
     <>
